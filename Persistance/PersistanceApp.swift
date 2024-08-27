0bed6fdd5687db1315fa2165be10d8edc778f5de
Ramen-4life <quincyalex09@gmail.com>
2024-08-27 09:21:53 -0500

    Initial Commit

---
+++ Persistance/PersistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistanceApp.swift
+//  Persistance
+//
+//  Created by Quincy Alex on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PersistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
